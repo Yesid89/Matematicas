b19524e087eb4cb0ab4422392411ec153986d765
YL <desarrollosyesid@gmail.com>
2023-01-16 19:26:56 -0500

    c2

--- areas.py
+++ areas.py
@@ -2,3 +2,13 @@ def areaCuadrado():
     lado = float(input())
     return print(lado ** 2)
 areaCuadrado
+
+pi = 3,1416
+def areaCirculo():
+    radio = float(input("Ingrese el valor del radio: "))
+    altura = float(input("Ingrese el valor de la altura: "))
+    return print("El area del Rectangulo es: ", pi * radio ** 2)
+areaCirculo()
+
+areaCirculo()
+
