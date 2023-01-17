8e6d32563698de81a572b88765c616b25971e0d1
Yesid89 <33935369+Yesid89@users.noreply.github.com>
2023-01-16 19:59:43 -0500

    Update areas.py

--- areas.py
+++ areas.py
@@ -1,12 +1,18 @@
-#def areaCuadrado():
-#    lado = float(input())
-#    return print(lado ** 2)
-#areaCuadrado
+def areaCuadrado ():
+    lado= float(input("ingrese el valor de l lado: "))
+    return print( "El area del cuadrado es: " , lado **2) # 16 de Julio de 2023
+areaCuadrado() # 17n de julio de 2023
 
-pi = 3.1416
-def areaCirculo():
-    radio = float(input("Ingrese el valor del radio: "))
-    altura = float(input("Ingrese el valor de la altura: "))
-    return print("El area del Rectangulo es: ", pi * radio ** 2)
-areaCirculo()
+def areaRectangulo ():
+    base= float(input("ingrese el valor de la base: "))
+    altura= float(input("ingrese el valor de la altura: "))
+    return print( "El area del Rectangulo es: " , base * altura) # 16 de Julio de 2023
+areaRectangulo() # 18 de julio de 2023
 
+pi= 3,1416
+
+def areaCirculo ():
+    radio= float(input("ingrese el valor de la radio: "))
+    altura= float(input("ingrese el valor de la altura: "))
+    return print( "El area del Rectangulo es: " , pi *radio **2) # 16 de Julio de 2023
+areaCirculo() # 18 de julio de 2023
