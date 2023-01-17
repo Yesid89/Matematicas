ec9972ed87614bd9198cf8718cf2d2e0565e3233
Yesid89 <33935369+Yesid89@users.noreply.github.com>
2023-01-16 20:00:38 -0500

    Update perimetro.py

--- perimetro.py
+++ perimetro.py
@@ -1,19 +1,18 @@
-def perimetro():
-    pass
-perimetro()
+def perCuadrado ():
+    lado= float(input("ingrese el valor de perimetro : "))
+    return print( "El area del cuadrado es: " , lado *4) # 16 de Julio de 2023
+perCuadrado() # 30 de julio de 2023
 
+def perRectangulo ():
+    base= float(input("ingrese el valor de la base: "))
+    altura= float(input("ingrese el valor de la altura: "))
+    return print( "El area del Rectangulo es: " , 2 * base + 2* altura) # 16 de Julio de 2023
+perRectangulo() # 31 de julio de 2023
 
-pi = 3.1416
-def perRectangulo():
-    base = float(input("Ingrese el valor de la base: "))
-    altura = float(input("Ingrese el valor de la altura: "))
-    return print("El area del perRectangulo es: ", 2 * base + 2* altura)
-perRectangulo()
+pi= 3,1416
 
-def perCirculo():
-    radio = float(input("Ingrese el valor del radio: "))
-    altura = float(input("Ingrese el valor de la altura: "))
-    return print("El area del perRectangulo es: ", 2 + pi * radio)
-perCirculo()
-
-
+def perCirculo ():
+    radio= float(input("ingrese el valor de la radio: "))
+    altura= float(input("ingrese el valor de la altura: "))
+    return print( "El area del Rectangulo es: " , 2+ pi *radio) # 16 de Julio de 2023
+perCirculo() # 31 de julio de 2023
