185af2837fd08bc09d8d1199d233cfa943e1dd33
YL <desarrollosyesid@gmail.com>
2023-01-16 19:43:36 -0500

    c5

--- areas.py
+++ areas.py
@@ -1,14 +1,12 @@
-def areaCuadrado():
-    lado = float(input())
-    return print(lado ** 2)
-areaCuadrado
+#def areaCuadrado():
+#    lado = float(input())
+#    return print(lado ** 2)
+#areaCuadrado
 
-pi = 3,1416
+pi = 3.1416
 def areaCirculo():
     radio = float(input("Ingrese el valor del radio: "))
     altura = float(input("Ingrese el valor de la altura: "))
     return print("El area del Rectangulo es: ", pi * radio ** 2)
-areaCirculo()
-
 areaCirculo()
 
--- perimetro.py
+++ perimetro.py
@@ -1,3 +1,19 @@
 def perimetro():
     pass
 perimetro()
+
+
+pi = 3.1416
+def perRectangulo():
+    base = float(input("Ingrese el valor de la base: "))
+    altura = float(input("Ingrese el valor de la altura: "))
+    return print("El area del perRectangulo es: ", 2 * base + 2* altura)
+perRectangulo()
+
+def perCirculo():
+    radio = float(input("Ingrese el valor del radio: "))
+    altura = float(input("Ingrese el valor de la altura: "))
+    return print("El area del perRectangulo es: ", 2 + pi * radio)
+perCirculo()
+
+
